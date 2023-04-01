d1373bcc6a0b8111bcebe2bf3dd929470ec58e5d
Jordan Capital <dicksonmokua19@gmail.com>
2023-04-01 07:01:28 -0500

    dispatch feeling

--- src/components/FeelingForm/FeelingForm.jsx
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -7,7 +7,39 @@ function Feeling () {
     const history = useHistory();
     const [feeling, setFeeling] = useState("");
 
+const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Feeling', feeling);
 
+    dispatch({
+        type: 'ADD_FEELING',
+        payload: feeling,
+    });
 
-    
+    setFeeling('');
+    history.push('/understanding');
+};
+
+return (
+    <div>
+        <h2>How are you feeling today?</h2>
+        <form onSubmit = {handleSubmit}>
+            <lable>
+                Feeling?
+                <input
+                type="number"
+                min="1"
+                max="5"
+                value={feeling}
+                onChange={(event) => setFeeling(event.target.value)}
+                required   
+                />
+            </lable>
+            <br />
+            <button type="submit">Next</button>
+        </form>
+    </div>
+);
+
 }
+export default Feeling;
