3bb265e80315e0c42cd1aa13168af5116be20935
Jordan Capital <dicksonmokua19@gmail.com>
2023-04-01 06:45:40 -0500

    Create feeling component

---
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function Feeling () {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [feeling, setFeeling] = useState("");
+
+
+
+    
+}
