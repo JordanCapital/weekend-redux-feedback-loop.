3bfe2681ad487ab940f39e1ff0b25c2eff5dd800
Jordan Capital <dicksonmokua19@gmail.com>
2023-04-01 21:38:00 -0500

    button with on click event to trigger handleback function

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -5,5 +5,19 @@ import { useHistory } from 'react-router-dom';
 function BackButton () {
     const dispatch = useDispatch();
     const history = useHistory();
-    const feeling =useSelector(store => store.feeling)
+    const feeling = useSelector(store => store.feeling);
+    const understanding = useSelector(store => store.understanding);
+    const support = useSelector(store => store.support);
+    const comments = useSelector(store => store.comments);
+
+    const handleBack = () => { 
+
+    }
+// a button with onClick event that triggers handleBack function and is disabled if feeling value is empty
+    return (
+        <button onClick={handleBack} disabled={!feeling}>
+         Back
+        </button>
+    );
 }
+export default BackButton;
